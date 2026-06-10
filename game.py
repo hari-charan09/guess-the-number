a6ab2dc4d1565bbf0e7a1108ef388c08bb26bd6d
Hari Charan <haricharankanukuntla@gmail.com>
2026-06-10 19:03:58 +0530

    Added a functional tracking feature for number of attempts

--- game.py
+++ game.py
@@ -2,8 +2,10 @@ import random
 secret_number= random.randint(1,100)
 print("Welcome to Guess the Number! I am thinking of a number between 1 and 100.")
 guessed_correct=False
+attempts=0
 while not guessed_correct:
   guess=int(input("Take a guess: "))
+  attempts=attempts+1
   if guess > secret_number:
     print("Too high! Try guessing a lower number")
   elif guess<secret_number:
@@ -11,3 +13,4 @@ while not guessed_correct:
   else:
     print("Congratulations! You guessed the secret number!")
     guessed_correct=True
+print("you took",attempts," attempts to guess")
